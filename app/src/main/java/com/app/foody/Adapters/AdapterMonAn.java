3164bcbf3298aacf25cb76232edc8fd988426d39
Phạm Tiến Long <phamtienlong0309@gmail.com>
2020-12-21 14:40:20 +0700

    Tăng giảm số lượng
    Xử lý tăng giảm số lượng món ăn.

--- app/src/main/java/com/app/foody/Adapters/AdapterMonAn.java
+++ app/src/main/java/com/app/foody/Adapters/AdapterMonAn.java
@@ -1,9 +1,11 @@
 package com.app.foody.Adapters;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -33,9 +35,37 @@ public class AdapterMonAn extends RecyclerView.Adapter<AdapterMonAn.HolderMonAn>
     }
 
     @Override
-    public void onBindViewHolder(@NonNull HolderMonAn holder, int position) {
+    public void onBindViewHolder(@NonNull final HolderMonAn holder, int position) {
         MonAnModel monAnModel = monAnModelList.get(position);
         holder.txtTenMonAn.setText(monAnModel.getTenmon());
+
+        holder.txtSoLuong.setTag(0);
+
+        //img Tang so luong
+        holder.imgTangSoLuong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int dem = Integer.parseInt(holder.txtSoLuong.getTag().toString());
+                dem++;
+                holder.txtSoLuong.setText(dem + "");
+                holder.txtSoLuong.setTag(dem);
+
+            }
+        });
+
+        //img Giam so luong
+        holder.imgGiamSoLuong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int dem = Integer.parseInt(holder.txtSoLuong.getTag().toString());
+                if(dem != 0){
+                    dem--;
+                }
+                holder.txtSoLuong.setText(dem + "");
+                holder.txtSoLuong.setTag(dem);
+
+            }
+        });
     }
 
     @Override
@@ -44,11 +74,15 @@ public class AdapterMonAn extends RecyclerView.Adapter<AdapterMonAn.HolderMonAn>
     }
 
     public class HolderMonAn extends RecyclerView.ViewHolder {
-        TextView txtTenMonAn;
+        TextView txtTenMonAn, txtSoLuong;
+        ImageView imgGiamSoLuong, imgTangSoLuong;
 
         public HolderMonAn(@NonNull View itemView) {
             super(itemView);
             txtTenMonAn = (TextView) itemView.findViewById(R.id.txtTenMonAn);
+            txtSoLuong = (TextView) itemView.findViewById(R.id.txtSoLuong);
+            imgGiamSoLuong = (ImageView) itemView.findViewById(R.id.imgGiamSoLuong);
+            imgTangSoLuong = (ImageView) itemView.findViewById(R.id.imgTangSoLuong);
 
         }
     }
