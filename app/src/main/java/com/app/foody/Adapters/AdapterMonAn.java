5d284c6d4f370cb1e4253cc39fa004d87bd00ebf
Phạm Tiến Long <phamtienlong0309@gmail.com>
2020-12-21 15:52:31 +0700

    Lưu các món ăn đã đặt
    Lưu các món ăn đã đặt vào List

--- app/src/main/java/com/app/foody/Adapters/AdapterMonAn.java
+++ app/src/main/java/com/app/foody/Adapters/AdapterMonAn.java
@@ -11,32 +11,33 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.app.foody.Model.DatMon;
 import com.app.foody.Model.MonAnModel;
 import com.app.foody.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AdapterMonAn extends RecyclerView.Adapter<AdapterMonAn.HolderMonAn> {
     Context context;
     List<MonAnModel> monAnModelList;
+    public static List<DatMon> datMonList = new ArrayList<>();
 
     public AdapterMonAn(Context context, List<MonAnModel> monAnModelList){
         this.context = context;
         this.monAnModelList = monAnModelList;
     }
 
-
     @NonNull
     @Override
     public HolderMonAn onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.custom_layout_monan,parent,false);
-
         return new HolderMonAn(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull final HolderMonAn holder, int position) {
-        MonAnModel monAnModel = monAnModelList.get(position);
+        final MonAnModel monAnModel = monAnModelList.get(position);
         holder.txtTenMonAn.setText(monAnModel.getTenmon());
 
         holder.txtSoLuong.setTag(0);
@@ -50,6 +51,24 @@ public class AdapterMonAn extends RecyclerView.Adapter<AdapterMonAn.HolderMonAn>
                 holder.txtSoLuong.setText(dem + "");
                 holder.txtSoLuong.setTag(dem);
 
+                //list dat mon
+                DatMon datMonTag = (DatMon) holder.imgGiamSoLuong.getTag();
+                if(datMonTag != null){
+                    AdapterMonAn.datMonList.remove(datMonTag);
+                }
+
+                DatMon datMon =  new DatMon();
+                datMon.setSoLuong(dem);
+                datMon.setTenMonAn(monAnModel.getTenmon());
+
+                holder.imgGiamSoLuong.setTag(datMon);
+
+                AdapterMonAn.datMonList.add(datMon);
+                for(DatMon datMon1 : AdapterMonAn.datMonList){
+                    Log.d("kiemtra",""+ datMon1.getTenMonAn() + " - " + datMon1.getSoLuong());
+                }
+
+
             }
         });
 
@@ -60,7 +79,14 @@ public class AdapterMonAn extends RecyclerView.Adapter<AdapterMonAn.HolderMonAn>
                 int dem = Integer.parseInt(holder.txtSoLuong.getTag().toString());
                 if(dem != 0){
                     dem--;
+                    if(dem == 0){
+                        DatMon datMon = (DatMon) v.getTag();
+                        AdapterMonAn.datMonList.remove(datMon);
+//                        Log.d("kiemtra",""+ datMonList.size());
+                    }
                 }
+
+
                 holder.txtSoLuong.setText(dem + "");
                 holder.txtSoLuong.setTag(dem);
 
